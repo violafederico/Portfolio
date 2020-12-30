0fecd7c33b2e04da82e3c88dc14b3b30756be116
violafederico <fedev93@gmail.com>
2020-12-30 22:24:30 +0100

    about page changed css + responsive layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Homepage from './components/Home/Homepage';
 import './App.css'
+
 function App() {
   return (
     <div className="App">
--- src/components/Content/About/About.js
+++ src/components/Content/About/About.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 import { FaReact, FaCss3Alt, FaHtml5, FaJsSquare, FaBootstrap, FaNodeJs } from 'react-icons/fa'
 import { DiJqueryLogo } from 'react-icons/di'
 import { SiMysql } from 'react-icons/si'
 
+import skills from './skill.json'
 import './About.css'
-
 import me from "../../../assets/image/me.jpg"
 
 function About() {
+    const icons = [ <FaReact/>, <FaHtml5/>, <FaCss3Alt/>, <FaJsSquare/>, <FaBootstrap/>, <FaNodeJs/>, <DiJqueryLogo/>, <SiMysql/> ];
+    
     return (
         <div className="about">
             <h2>About me</h2>
@@ -21,77 +25,91 @@ function About() {
                 </div>
                 <div className="description">
                     <p>
-                        Sono uno studente di Informatica presso l'università di Catania in Sicilia ed ho da poco ultimato
-                        un corso di formazione di sviluppo fornt-end in React offerto dalla 
-                        <a href="https://tree.it/school/" target="_blank"> Tree School </a>.
+                        Vivo in Sicilia e sono uno studente di Informatica presso l'università di Catania.
                     </p>
+                    <p> Ho da poco seguito un corso di formazione in sviluppo fornt-end in React offerto dalla 
+                        <a href="https://tree.it/school/" target="_blank" rel="noreferrer"> Tree School </a>.</p>
                     <p>
                         Ho studiato diversi linguaggi dal C/C++ con le strutture dati LIFO/FIFO come Alberi, Pile, Code e Grafi
                         per passare poi a Java con i Servlet e web server e la programmazione distribuita ed nell'ultimo periodo
                         ho maturato interesse per le web app ed ho inziato a lavora con HTML5, CSS3 e JS(ES6).
                     </p>
-                    <small>Vedi la sezione <a href="/projects"> Progetti </a> 
+                    <small>Vedi la sezione  <Link to="/projects"> Projects </Link>
                         per vedere nel dettaglio i linguaggi trattati.</small>
                         
                     <div className="skills">
                         <h3>Developers Skills</h3>
                         <div className="container">
-                            <div className="barcont"> 
+                            {   skills.map( (skill, i) => {
+                                return (
+                                    <div key={i} className="barcont" >
+                                        <div className="bar" style={{width: skill.livello}}>
+                                            <div className="progress" ></div>
+                                        </div>
+                                       <span className="label" style={{color: skill.colore}}>
+                                            {icons[i]}<span> &nbsp; </span>
+                                           { skill.nome } </span>
+                                    </div>
+                                )
+                            })
+                                
+                            }
+                            {/* <div className="barcont"> 
                             <div className="bar" style={{width: "50%"}}>
                                     <div className="progress" ></div>
                                 </div>
                                 <span className="label" style={{color: "#61DBFB"}}><FaReact/>  React</span>
                             </div>
                             <div className="barcont" >
-                                <div className="bar" style={{width: "60%"}}>
+                                <div className="bar" style={{width: ""}}>
                                     <div className="progress" ></div>
                                 </div>
-                                <span className="label" style={{color: "#e34c26"}}><FaHtml5/>  HTML5  </span>
+                                <span className="label" style={{color: ""}}>    </span>
                             </div>
                             <div className="barcont" >
-                                <div className="bar" style={{width: "50%"}}>
+                                <div className="bar" style={{width: ""}}>
                                     <div className="progress" ></div>
                                 </div>
-                                <span className="label" style={{color: "#ebebeb"}}><FaCss3Alt/> CSS3 </span>
+                                <span className="label" style={{color: ""}}> </span>
                             </div>
                             <div className="barcont" >
-                                <div className="bar" style={{width: "70%"}}>
+                                <div className="bar" style={{width: ""}}>
                                     <div className="progress" ></div>
                                 </div>
-                                <span className="label" style={{color: "#f0db4f"}}><FaJsSquare/>  JS (JSX) </span>
+                                <span className="label" style={{color: ""}}>   </span>
                             </div>
                             <div className="barcont" >
-                                <div className="bar" style={{width: "45%"}}>
+                                <div className="bar" style={{width: ""}}>
                                     <div className="progress" ></div>
                                 </div>
-                                <span className="label" style={{color: "#563d7c"}}><FaBootstrap/> Boostrap </span>
+                                <span className="label" style={{color: ""}}></span>
                             </div>                                                     
                         </div>
                         <div className="container">
                             <div className="barcont"> 
-                            <div className="bar" style={{width: "30%"}}>
+                            <div className="bar" style={{width: ""}}>
                                     <div className="progress" ></div>
                                 </div>
-                                <span className="label" style={{color: "#68A063"}}><FaNodeJs/>  NodeJs</span>
+                                <span className="label" style={{color: ""}}>  </span>
                             </div>
                             <div className="barcont" >
-                                <div className="bar" style={{width: "20%"}}>
+                                <div className="bar" style={{width: ""}}>
                                     <div className="progress" ></div>
                                 </div>
-                                <span className="label" style={{color: "#131B28"}}><DiJqueryLogo/>  jQuery  </span>
+                                <span className="label" style={{color: ""}}>    </span>
                             </div>
                             <div className="barcont" >
-                                <div className="bar" style={{width: "35%"}}>
+                                <div className="bar" style={{width: ""}}>
                                     <div className="progress" ></div>
                                 </div>
-                                <span className="label" style={{color: "#F29111"}}><SiMysql/> MySQL </span>
+                                <span className="label" style={{color: ""}}> </span>
                             </div>
                             <div className="barcont" >
                                 <div className="bar" style={{width: "85%"}}>
                                     <div className="progress" ></div>
                                 </div>
                                 <span className="label" style={{color: "#f0db4f"}}>  ???? </span>
-                            </div>                           
+                            </div>                            */}
                         </div>
                     </div>
                 </div>
